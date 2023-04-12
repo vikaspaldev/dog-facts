214cea86a571797bc5a6b22ffdeda4d4a57a9803
Vikas Pal <6072449+vikaspaldev@users.noreply.github.com>
2023-04-12 06:35:51 -0400

    Add limitation in image count

--- src/services/dog.service.ts
+++ src/services/dog.service.ts
@@ -1,5 +1,7 @@
 const baseUrl = 'https://dog.ceo/api'
 
+const MAX_PHOTO_COUNT = 27
+
 export interface AllDogBreedResponse {
   breed: string
   subBreeds: string[]
@@ -15,6 +17,7 @@ export const fetchDogBreeds = async () => {
       breed,
       subBreeds: data.message[breed],
     }))
+    .slice(0, MAX_PHOTO_COUNT)
   return result as AllDogBreedResponse[]
 }
 
@@ -38,12 +41,12 @@ export const fetchDogSubBreedImage = async (
 export const fetchDogSubBreedByBreed = async (breed: string) => {
   const allBreeds = await fetchDogBreeds()
   const result = allBreeds.find(b => b.breed === breed)
-  return result?.subBreeds || []
+  return (result?.subBreeds || []).slice(0, MAX_PHOTO_COUNT)
 }
 
 export const fetchSubBreedImages = async (breed: string, subBreed: string) => {
   const data = await fetch(`${baseUrl}/breed/${breed}/${subBreed}/images`).then(
     r => r.json() as Promise<{ message: string[] }>,
   )
-  return data.message
+  return data.message.slice(0, MAX_PHOTO_COUNT)
 }
